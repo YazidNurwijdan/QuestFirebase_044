36810a72f53e49cb6fc8716627e21f80e1b2a074
YazidNurwijdan <ynurwijdan@gmail.com>
2026-01-03 03:26:14 +0700

    menambah dan mengisi file DestinasiEntry

---
+++ app/src/main/java/com/example/pam14/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.pam14.view.route
+
+import com.example.pam14.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
